6723708c0157bb86a65e474af26a17625110007b
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2023-03-18 17:52:29 +0530

    fixed error: posts route backend

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import path from 'path';
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
+import postRoutes from './routes/posts.js'
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
@@ -49,7 +50,7 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-// app.use("/posts", postRoutes);
+app.use("/posts", postRoutes);
 
 const PORT = process.env.PORT || 6001;
 mongoose
