04b499ddb9ed4cd6ca3511f768d00755d7fdfa0e
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2023-03-18 19:37:44 +0530

    friend list updated

--- frontend/src/scenes/homePage/index.jsx
+++ frontend/src/scenes/homePage/index.jsx
@@ -7,7 +7,7 @@ import UserWidget from 'scenes/widgets/UserWidget'
 import MyPostWidget from 'scenes/widgets/MyPostWidget'
 import PostsWidget from "scenes/widgets/PostsWidget";
 // import AdvertWidget from "scenes/widgets/AdvertWidget";
-// import FriendListWidget from "scenes/widgets/FriendListWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)")
@@ -37,7 +37,8 @@ const HomePage = () => {
           <Box flexBasis="26%">
             {/* <AdvertWidget />
             <Box m="2rem 0" />
-            <FriendListWidget userId={_id} /> */}
+            // <FriendListWidget userId={_id} /> */}
+            <FriendListWidget userId={_id} />
           </Box>
         )}
       </Box>
