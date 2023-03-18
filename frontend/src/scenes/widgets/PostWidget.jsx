1d468448133a9118df9432b350648dd649f42ca1
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2023-03-18 07:44:58 +0530

    friend component added

---
+++ frontend/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+    ChatBubbleOutlineOutlined,
+    FavoriteBorderOutlined,
+    FavoriteOutlined,
+    ShareOutlined,
+  } from "@mui/icons-material";
+  import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+  import FlexBetween from "components/FlexBetween";
+//   import Friend from "components/Friend";
+  import WidgetWrapper from "components/WidgetWrapper";
+  import { useState } from "react";
+  import { useDispatch, useSelector } from "react-redux";
+  import { setPost } from "state";
+
+const PostWidget = () => {
+  return (
+    <div>PostWidget</div>
+  )
+}
+
+export default PostWidget
